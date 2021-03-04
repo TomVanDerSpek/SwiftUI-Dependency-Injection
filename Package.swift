9bfb0619a3399c266d61b47bc5ec5d6fa3a9aab0
Tom van der Spek <tom.vanderspek@moneyou.nl>
2021-03-04 17:31:00 +0100

    Update iOS minimum deployment target to 13.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftUIDI",
     platforms: [
-        .macOS(.v10_15), .iOS(.v11), .tvOS(.v13)
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13)
     ], products: [
         .library(
             name: "SwiftUIDI",
